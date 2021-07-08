867eb981543d0349f3bbf9e7d1a9c672c1e9d826
Aishwarya Singh <holaamigosaishu@gmail.com>
2021-07-08 16:41:11 +0530

    added composer.json file

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div>
+    <h1>Get in Touch</h1>
+    <p>Please Fill in the fields</p>
+    <form action="contact.php" method="post">
+    <input type="text" name="name" placeholder= "Full Name">
+    <input type="email" name="email" placeholder="E-mail" >
+    <input type="text" name="subject" placeholder = "Subject">
+    <textarea name="message" placeholder="Enter the message"></textarea>
+    <button type="submit" name="submit">SEND E-Mail</button>
+
+
+    </form>
+    </div>
+    
+</body>
+</html>
